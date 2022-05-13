e0dfa97bcbef96ee0f65e4459183dc4d8096bb42
kakemos <cjkbsn@gmail.com>
2022-05-13 12:29:59 +0200

    Add bottom nav bar

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import { FrontPage } from "./pages/frontPage/frontPage";
 import { Diary } from "./pages/diaryPage/diary";
 import { Profile } from "./pages/profilePage/profile";
@@ -9,22 +9,39 @@ import { FriendsActivity } from "./pages/friendsActivityPage/friendsActivity";
 import { FriendPage } from "./pages/friendsPage/friendPage";
 import { NewProfile } from "./pages/registerUserPage/newProfile";
 import { LoginPage } from "./pages/loginPage/loginPage";
+import "./index.css"
+
+function NavBar() {
+  return (<>
+    <div id={"nav-bar"}>
+      <Link to={"/diary"}>Diary</Link>
+      <Link to={"/session"}>Session</Link>
+      <Link to={"/friends-activity"}>Friends Activity</Link>
+    </div>
+  </>)
+}
 
 function Application() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/register"} element={<NewProfile />} />
-        <Route path={"/login/*"} element={<LoginPage />} />
-        <Route path={"/diary"} element={<Diary />} />
-        <Route path={"/profile"} element={<Profile />} />
-        <Route path={"/session"} element={<Session />} />
-        <Route path={"/friends-activity"} element={<FriendsActivity />} />
-        <Route path={"/friend-page"} element={<FriendPage />} />
-      </Routes>
+  return (<>
+      <BrowserRouter>
+        <main>
+          <Routes>
+            <Route path={"/"} element={<FrontPage />} />
+            <Route path={"/register"} element={<NewProfile />} />
+            <Route path={"/login/*"} element={<LoginPage />} />
+            <Route path={"/diary"} element={<Diary />} />
+            <Route path={"/profile"} element={<Profile />} />
+            <Route path={"/session"} element={<Session />} />
+            <Route path={"/friends-activity"} element={<FriendsActivity />} />
+            <Route path={"/friend-page"} element={<FriendPage />} />
+          </Routes>
+        </main>
+
+        <footer>
+          <NavBar />
+        </footer>
     </BrowserRouter>
-  );
+  </>);
 }
 
 ReactDOM.render(<Application />, document.getElementById("app"));
