2525f129300bdc98c4421811811cab483b7a2f53
tonjehusvik <tonje.husvik@hotmail.com>
2022-05-13 12:13:32 +0200

    Edited react version from 18 tp 17 and added bodyparser and dotenv to server.js

--- client/pages/profilePage/profile.jsx
+++ client/pages/profilePage/profile.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export function Profile() {
-  return <h1>Profile</h1>;
+  return (
+    <div>
+      <h1>Profile photo / add new photo - function</h1>
+      <EditUserButton label={"Rediger profil"} />
+      <h3>Simen sjømann</h3>
+      <p id="aboutMe">Litt om seg selv..</p>
+      <p id="age">24</p>
+      <p id="education">IT</p>
+      <p id="school">Høyskolen Kristiania</p>
+    </div>
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,22 @@
 import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.static("../client/dist"));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Started on http://localhost:${server.address().port}`);
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
