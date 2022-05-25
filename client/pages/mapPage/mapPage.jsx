b50cd778f0e66cb0aa362eda1e0df8d5acc4a007
tonjehusvik <tonje.husvik@hotmail.com>
2022-05-25 22:13:18 +0200

    added marker to map

--- client/pages/mapPage/mapPage.jsx
+++ client/pages/mapPage/mapPage.jsx
@@ -4,6 +4,7 @@ import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 export function MapPage() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCBa-5tK7ycBCJwSEXvhaAy9q_pfN4f8Ww",
+    //Have to put the Api key into .env later (it doesnt work now somehow)
   });
 
   if (!isLoaded) return <div>Loading...</div>;
@@ -11,13 +12,16 @@ export function MapPage() {
 }
 
 function Map() {
+  /*
+  GoogleMap is the dependency where we get the map from and can edit zoom and center. map-container the size adjusted in the CSS-file
+  Marker is the position for the marker (duh), it is hardcoded to be in Oslo sentrum.*/
   return (
     <GoogleMap
       zoom={14}
       center={{ lat: 59.911491, lng: 10.757933 }}
       mapContainerClassName="map-container"
     >
-      <Marker />
+      <Marker position={{ lat: 59.911491, lng: 10.757933 }} />
     </GoogleMap>
   );
 }
