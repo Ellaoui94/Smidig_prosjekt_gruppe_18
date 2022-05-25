3afa7f46b900ce9522dee425f09a31df4fa0ffcc
tonjehusvik <tonje.husvik@hotmail.com>
2022-05-25 15:34:28 +0200

    figuring out map function

--- client/pages/mapPage/mapPage.jsx
+++ client/pages/mapPage/mapPage.jsx
@@ -1,3 +1,27 @@
+import { GoogleApiWrapper, InfoWindow, Map, Marker } from "google-maps-react";
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCBa-5tK7ycBCJwSEXvhaAy9q_pfN4f8Ww",
+})(MapContainer);
+
+export function MapContainer() {
+  return (
+    <>
+      <Map google={google} zoom={14}></Map>
+      <Marker onClick={onMarkerClick} name={"Current location"} />
+      <InfoWindow onClose={onInfoWindowClose}>
+        <div>
+          <h1></h1>
+        </div>
+      </InfoWindow>
+    </>
+  );
+}
+
 export function MapPage() {
-  return <h1>Map comes here</h1>;
+  return (
+    <>
+      <h1>Map comes here</h1>;
+    </>
+  );
 }
