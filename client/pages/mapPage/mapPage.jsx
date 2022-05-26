bc1bbdb0c912bd7f8e1796783230cb52e7500a10
tonjehusvik <tonje.husvik@hotmail.com>
2022-05-26 14:16:29 +0200

    Everything (location, status, icon, filter, find other) has hardkoded values but this ish close to what we want it to look like.

--- client/pages/mapPage/mapPage.jsx
+++ client/pages/mapPage/mapPage.jsx
@@ -1,5 +1,10 @@
 import { useMemo } from "react";
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 
 export function MapPage() {
   //connects to .env file
@@ -16,16 +21,41 @@ function Map() {
   GoogleMap is the dependency where we get the map from and can edit zoom and center. map-container the size adjusted in the CSS-file
   Marker is the position for the marker (duh), it is hardcoded to be in Oslo sentrum.*/
   return (
-    <GoogleMap
-      zoom={14}
-      center={{ lat: 59.911491, lng: 10.757933 }}
-      mapContainerClassName="map-container"
-    >
-      <Marker
-        title={"does this work"}
-        name={"or this????"}
-        position={{ lat: 59.911491, lng: 10.757933 }}
-      />
-    </GoogleMap>
+    <>
+      <div className={"filter-by-course"}>
+        <h3>Finn andre studenter</h3>
+        <h4>Filtrer etter emne</h4>
+        <div className={"flex-container"}>
+          <div className={"flex-item"}>Digital teknologi</div>
+          <div className={"flex-item"}>Filosofi</div>
+          <div className={"flex-item"}>Design</div>
+          <div className={"flex-item"}>Programmering</div>
+          <div className={"flex-item"}>Se alle</div>
+        </div>
+      </div>
+      <GoogleMap
+        zoom={14}
+        center={{ lat: 59.911491, lng: 10.757933 }}
+        mapContainerClassName="map-container"
+      >
+        <Marker
+          icon={{
+            url: "https://cdn.dribbble.com/users/2638821/screenshots/9213234/media/d51ac680af06d6c9642b145ebe24633f.jpg?compress=1&resize=400x300",
+            scaledSize: { width: 70, height: 70 },
+          }}
+          position={{ lat: 59.911481, lng: 10.757923 }}
+          anchor={50}
+        >
+          <div>hva er dette og hvor går den</div>
+          <InfoWindow position={{ lat: 59.911481, lng: 10.757923 }}>
+            <div className={"info-window"}>
+              <h3>Navn navnesen</h3>
+              <p>Programmering</p>
+              <p>Join gjerne! Jobber med innlevering 2</p>
+            </div>
+          </InfoWindow>
+        </Marker>
+      </GoogleMap>
+    </>
   );
 }
