746fd4abd01c6b85ca07159c8ab257afe0ca4969
tonjehusvik <tonje.husvik@hotmail.com>
2022-05-31 22:15:16 +0200

    Trying to filter in map by subjects

--- client/pages/mapPage/listSubjects.jsx
+++ client/pages/mapPage/listSubjects.jsx
@@ -1,5 +1,109 @@
-import React, { useContext, useState } from "react";
-import { UserApiContext } from "../../userApiContext";
+import React, { useEffect, useState } from "react";
+import { StudyDesign } from "./mapPage";
+import { StudyPhilosophy } from "./mapPage";
+import { StudyDigitalTechnology } from "./mapPage";
+import { StudyProgramming } from "./mapPage";
+
+/* filter subjects with use of buttons */
+export function ListSubjects() {
+  const [designSelected, setDesignSelected] = useState(false);
+  const [digitalTechnologySelected, setDigitalTechnologySelected] =
+    useState(false);
+  const [philosophySelected, setPhilosophySelected] = useState(false);
+  const [programmingSelected, setProgrammingSelected] = useState(false);
+
+  const [selectedSubject, setSelectedSubject] = useState([]);
+
+  useEffect(() => {
+    const filteredSubjects = () => {
+      if (digitalTechnologySelected) {
+        return StudyDigitalTechnology();
+      }
+      if (designSelected) {
+        return StudyDesign();
+      }
+      if (programmingSelected) {
+        return StudyProgramming();
+      }
+      if (philosophySelected) {
+        return StudyPhilosophy();
+      }
+    };
+    setSelectedSubject(filteredSubjects);
+  }, [
+    digitalTechnologySelected,
+    designSelected,
+    programmingSelected,
+    philosophySelected,
+  ]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setSelectedSubject(selectedSubject);
+  }
+
+  const handleDigTekButton = () => {
+    setDigitalTechnologySelected(!digitalTechnologySelected);
+  };
+
+  const handlePhilosophyButton = () => {
+    setPhilosophySelected(!philosophySelected);
+  };
+
+  const handleDesignButton = () => {
+    setDesignSelected(!designSelected);
+  };
+
+  const handleProgrammingButton = () => {
+    setProgrammingSelected(!programmingSelected);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className={"filter-by-course"}>
+          <h3>Finn andre studenter</h3>
+          <h4>Filtrer etter emne</h4>
+          <div className={"flex-container"}>
+            <div className={"flex-item"}>
+              <button
+                className={digitalTechnologySelected ? "clicked" : null}
+                onClick={handleDigTekButton}
+              >
+                Digital teknologi
+              </button>
+              <button
+                className={philosophySelected ? "clicked" : null}
+                onClick={handlePhilosophyButton}
+              >
+                Filosofi
+              </button>
+              <button
+                className={designSelected ? "clicked" : null}
+                onClick={handleDesignButton}
+              >
+                Design
+              </button>
+              <button
+                className={programmingSelected ? "clicked" : null}
+                onClick={handleProgrammingButton}
+              >
+                Programmering
+              </button>
+              <button>Se alle</button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+}
+/* SKROTET
+{subjectView.map((subjects) => (
+   <>
+      <SubjectCard key={subjects.subject} subjects={subjects} />
+   </>
+))}
 
 const subjectView = [
   {
@@ -28,32 +132,4 @@ function SubjectCard({ subjects: { subject } }) {
     </>
   );
 }
-
-/* Taken from Johannes' lesson 12. Not finnished. But the thought is to filter subjects with use of buttons */
-export function ListSubjects() {
-  const { listSubjects } = useContext(UserApiContext);
-  const [subject, setSubject] = useState("");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setSubject(subject);
-  }
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div className={"filter-by-course"}>
-          <h3>Finn andre studenter</h3>
-          <h4>Filtrer etter emne</h4>
-          <div className={"flex-container"}>
-            {subjectView.map((subjects) => (
-              <>
-                <SubjectCard key={subjects.subject} subjects={subjects} />
-              </>
-            ))}
-          </div>
-        </div>
-      </form>
-    </>
-  );
-}
+ */
