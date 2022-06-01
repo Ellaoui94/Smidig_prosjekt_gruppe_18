e0c851ff32f71b1509d935efd51f5350f06f7543
kakemos <cjkbsn@gmail.com>
2022-06-01 10:39:14 +0200

    Heroku fix

--- server/server.js
+++ server/server.js
@@ -12,12 +12,14 @@ import { UsersRoutes } from "./routes/users.js";
 import { StudySessionApi } from "./routes/studySessions.js";
 import { CourseRoutes } from "./routes/course.js";
 import { ContactInfoApi } from "./routes/contactInfo.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
-
-app.use(express.static("../client/dist"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.resolve(__dirname, "..", "client", "dist")));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors());
