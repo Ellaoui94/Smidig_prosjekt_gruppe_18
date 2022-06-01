2de3ea8565b545ca07b0a7731a5b54d6f678032f
kakemos <cjkbsn@gmail.com>
2022-06-01 10:10:51 +0200

    Hide todo from frontapge in the meantime

--- client/pages/mainPage/cards/finishedSessionsCard.jsx
+++ client/pages/mainPage/cards/finishedSessionsCard.jsx
@@ -52,7 +52,7 @@ export function FinishedSessionsCard() {
             {new Date(session.date).toLocaleDateString("no-NO", dateFormat)}
           </Link>
           <h5>{session.courseTitle}</h5>
-          <h6>{"- " + session.todos[0].todo}</h6>
+          {/*<h6>{"- " + session.todos[0].todo}</h6>*/}
           <div className={"arrow-div"}>
             <p>></p>
           </div>
