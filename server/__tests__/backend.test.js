5db8c369f37cfdd72f28cb97e1b98b0aae2eee9f
tonjehusvik <tonje.husvik@hotmail.com>
2022-06-08 23:25:16 +0200

    Fixing tests

--- server/__tests__/backend.test.js
+++ server/__tests__/backend.test.js
@@ -1,12 +1,40 @@
-import express from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
 import axios from "axios";
 
 describe("API connection", () => {
   it("should connect to localhost", async () => {
-    const response = await axios.get("http://localhost:3000/");
-    console.log(response.data);
+    const response = await axios.get("http://localhost:3000/", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     expect(response.status).toEqual(200);
+  });
+  it.only("should get planned sessions", async () => {
+    const response = await axios.get(
+      "http://localhost:3000/api/session/planned-session",
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    expect(response.status).toEqual(200);
+  });
+  it.only("should log in", async () => {
+    const response = await axios
+      .post(
+        "http://localhost:3000/api/auth/",
+        {
+          email: "tryingtotest@test.no",
+          password: "HorsePony69!Banana",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => res.data);
+    console.log(response);
   });
 });
